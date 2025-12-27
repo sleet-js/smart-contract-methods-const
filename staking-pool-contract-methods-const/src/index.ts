5831f41b6eb6e0789e004fb17227d103e9246fd8
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-27 10:31:25 -0600

    po

--- staking-pool-contract-methods-const/src/index.ts
+++ staking-pool-contract-methods-const/src/index.ts
@@ -2,3 +2,5 @@
 // ===== staking-pool-contract-methods-const =====
 export { factory_pool_methods_const } from "./factory_pool";
 export { factory_poolv1_methods_const } from "./factory_poolv1";
+export { pool_methods_const } from "./pool"
+export { poolv1_methods_const } from "./poolv1"
